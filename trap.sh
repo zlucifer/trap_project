25f5fb6e8b1efe6f0ec815058a743503d0149714
zlucifer <dekxter000@gmail.com>
2018-12-09 23:45:38 +0700

    Update trap.sh

--- trap.sh
+++ trap.sh
@@ -42,54 +42,69 @@ echo " mendapatkan IP atau lokasi Target"
 echo
 echo Selamat datang $nick ":)" Jones! :v
 echo 
-echo Apa kamu sudah membuat file trap?
-echo "y/n?"
-read confirm
-if [ $confirm = "y" ]; then
-    echo "Masukan nama file yang sudah kamu buat"
-    echo "Untuk melakukan pengecekan hasil"
-    read file
-    echo "Melakukan Pengecekan"
-    i=0
-    max=100
-while [ $i -le $max ]; do
-    echo -ne "\nProses Pengecekan : $i% "
-    sleep 0.03
-if [ $i -eq 100 ]; then
-    echo -ne " [complete!]\n"
-    get_url=$(curl -s http://zlucifer.com/api/trap.php)
-    curl -s $get_url/cek.php?input=$file #cek
-fi
-    let i++
-done
-elif [ $confirm = "n" ]; then
-    echo "Masukan nama file yang ingin kamu buat"
-    read create
-    echo "Memproses pembuatan file"
-    i=0
-    max=100
-while [ $i -le $max ]; do
-    echo -ne "\nProses Pembuatan : $i% "
-    sleep 0.03
-if [ $i -eq 100 ]; then
-    echo -ne " [complete!]\n"
-    get_url=$(curl -s http://zlucifer.com/api/trap.php)
-    curl -s $get_url/trap.php?create=$create #create
-fi
-    let i++
-done    
-    echo "ingin melakukan pengecekan?"
+get_url=$(curl -s http://zlucifer.com/api/trap.php) #cek status
+cek='curl -s '$get_url # check status
+response=`curl -s -o /dev/null -w "%{http_code}" $cek`
+if [[ $response != *GoogleDrive* ]]; then
+    echo
+    echo "Website Offline/Restart untuk sementara"
+else
+    echo "Apa kamu sudah membuat file trap?"
     echo "y/n?"
-    read cek_lagi
-if [ $cek_lagi = "y" ]; then
-    echo "Masukan nama file yang sudah kamu buat"
-    echo "Untuk melakukan pengecekan hasil"
-    read file
-    echo "Melakukan Pengecekan"
-    curl -s $get_url/cek.php?input=$file #cek
-elif [ $cek_lagi = "n" ]; then
-    echo "Terimakasih sudah menggunakan Trap Project :)"
-fi
-else 
-echo "Kesalahan"
+    read confirm
+
+
+    if [ $confirm = "y" ]; then
+        echo "Masukan nama file yang sudah kamu buat"
+        echo "Untuk melakukan pengecekan hasil"
+        read file
+        echo "Melakukan Pengecekan"
+        i=0
+        max=100
+    while [ $i -le $max ]; do
+        echo -ne "\nProses Pengecekan : $i% "
+        sleep 0.03
+    if [ $i -eq 100 ]; then
+        echo -ne " [complete!]\n"
+        get_url=$(curl -s http://zlucifer.com/api/trap.php)
+        curl -s $get_url/cek.php?input=$file #cek
+    fi
+        let i++
+    done
+
+
+    elif [ $confirm = "n" ]; then
+        echo "Masukan nama file yang ingin kamu buat"
+        read create
+        echo "Memproses pembuatan file"
+        i=0
+        max=100
+    while [ $i -le $max ]; do
+        echo -ne "\nProses Pembuatan : $i% "
+        sleep 0.03
+    if [ $i -eq 100 ]; then
+        echo -ne " [complete!]\n"
+        get_url=$(curl -s http://zlucifer.com/api/trap.php)
+        curl -s $get_url/trap.php?create=$create #create
+    fi
+        let i++
+    done    
+        echo "ingin melakukan pengecekan?"
+        echo "y/n?"
+        read cek_lagi
+    if [ $cek_lagi = "y" ]; then
+        echo "Masukan nama file yang sudah kamu buat"
+        echo "Untuk melakukan pengecekan hasil"
+        read file
+        echo "Melakukan Pengecekan"
+        curl -s $get_url/cek.php?input=$file #cek
+    elif [ $cek_lagi = "n" ]; then
+        echo "Terimakasih sudah menggunakan Trap Project :)"
+    fi
+
+
+
+    else 
+    echo "Kesalahan"
+    fi
 fi
